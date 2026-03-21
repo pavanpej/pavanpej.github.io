ad1dafa0dc87d619ce531609bb8aced2054a34ce
Pavan Rao <pavanpej@gmail.com>
2026-03-21 15:46:21 -0700

    feat: improvements
    * sitemap
    * robots.txt
    * ImageViewer improvements
    * additional build checks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pavanpej.com',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   output: 'static',
   build: {
     assets: 'assets',
